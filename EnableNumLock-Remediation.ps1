1750c9b4fc2ec5301bce2f41f164e63edb2d6d53
Andrew-c94 <andrew.currell@outlook.com>
2025-03-13 17:39:13 +0800

    Add files via upload

---
+++ EnableNumLock-Remediation.ps1
@@ -0,0 +1,49 @@
+﻿<#
+Script Name: EnableNumLock-Remediation.ps1
+Version: 1
+
+Description: This is a remediation script used with remediations to enable numb lock by default for all users.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+#------------------------------------ Set Variables -------------------------------------#
+
+$logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\EnableNumLock-Remediation.log"
+$Key = "HKEY_USERS\.DEFAULT\Control Panel\Keyboard"
+$ValueName = "InitialKeyboardIndicators"
+$Value = "2"
+#This sets Num Lock to enabled state.
+
+#------------------------------------ Start log file ------------------------------------#
+
+Start-Transcript -Path $logfile
+function WriteLog
+{
+Param ([string]$LogString)
+
+$DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$LogMessage = "$Datetime $LogString"
+
+Write-Host $LogMessage
+}
+
+#-------------------------------- Enable Num Lock ----------------------------------#
+
+#Check reg path exists and add it if not
+If (-NOT (Test-Path $Key)) {
+  New-Item -Path $Key -Force | Out-Null
+}  
+# Set reg value and catch errors if any
+Try{
+    New-ItemProperty -Path $Key -Name $ValueName -Value $Value -PropertyType DWORD -Force
+    WriteLog "Registry value has been added successfully"
+    Stop-Transcript
+    #Exit 0
+}
+catch {
+    $errMsg = $_.Exception.Message
+    return $errMsg
+    Stop-Transcript
+    #Exit 1
+}
