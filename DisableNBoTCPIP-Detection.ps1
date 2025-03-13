740d9dffc5da3ce6fa31b800a488ef76b492d89f
Andrew-c94 <andrew.currell@outlook.com>
2025-03-13 17:49:04 +0800

    Add files via upload

---
+++ DisableNBoTCPIP-Detection.ps1
@@ -0,0 +1,63 @@
+﻿<#
+Script Name: DisableNBoTCPIP-Detection.ps1
+Remediation Version: 1
+
+Description: This detection script checks all network interfaces present on the client and if it finds any with NetBIOS over TCP/IP enabled it triggers the remediation script to run.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+#------------------------------------ Set Variables -------------------------------------#
+
+$logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\DisableNBoTCPIP-Detection.log"
+$Key = "HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
+$ValueName = "NetbiosOptions"
+$ExpectedValue = "2" #This disables NetBIOS.
+
+#------------------------------------- Start log file -------------------------------------#
+
+Start-Transcript -Path $logfile
+function WriteLog
+{
+Param ([string]$LogString)
+
+$DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$LogMessage = "$Datetime $LogString"
+
+write-host $LogMessage
+}
+
+#----------------------------- Detect NetBIOS over TCP/IP Status -------------------------------#
+
+#Null variables
+$ValueMissing = $null
+$Interfaces = $null
+$Interface = $null
+
+#Get all child items into array
+$Interfaces = @(Get-ChildItem $Key)
+
+# Check for existence of registry key and specific value
+foreach ($Interface in $Interfaces) {
+    $InterfacePath = Join-Path -Path $Key -ChildPath $Interface.PSChildName
+    
+    if (Test-Path $InterfacePath) {
+        $value = Get-ItemProperty -Path $interfacePath -Name $valueName -ErrorAction SilentlyContinue
+        if ($value.$ValueName -eq $expectedValue) {
+            writelog "$ValueName at $interfacePath is set to $expectedValue as required."
+            $ValueMissing = $false
+        }
+        else {
+        writelog "Check failed, $ValueName on $Interface is not set to $ExpectedValue"
+        Stop-Transcript
+        Exit 1
+        }
+    }
+}
+
+# Exit script if without requiring Remediation if $ValueMissing is still $false
+if ($ValueMissing -eq $false) {
+    writelog "All interfaces have the correct NetBois setting, exiting without further Action"
+    Stop-Transcript
+    Exit 0
+    }
---
+++ DisableNBoTCPIP-Remediation.ps1
@@ -0,0 +1,65 @@
+﻿<#
+Script Name: DisableNBoTCPIP-Remediation.ps1
+Remediation Version: 1
+
+Description: This remediation script checks all network interfaces present on the client and if it finds any with NetBIOS over TCP/IP enabled it disables it.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+#------------------------------------ Set Variables -------------------------------------#
+
+$logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\DisableNBoTCPIP-Remediation.log"
+$Key = "HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
+$ValueName = "NetbiosOptions"
+$NewValue = "2" #This disables NetBIOS.
+$ErrorActionPreference = 'stop'
+
+#------------------------------------- Start log file -------------------------------------#
+
+Start-Transcript -Path $logfile
+function WriteLog
+{
+Param ([string]$LogString)
+
+$DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$LogMessage = "$Datetime $LogString"
+
+write-host $LogMessage
+}
+
+#-------------------------------- Disable NetBIOS over TCP/IP ----------------------------------#
+
+#Null variables
+$Interfaces = $null
+$Interface = $null
+
+#Get all child items into array
+$Interfaces = @(Get-ChildItem $Key)
+
+# Check for existence of registry key and specific value
+Try{
+foreach ($Interface in $Interfaces) {
+    $InterfacePath = Join-Path -Path $Key -ChildPath $Interface.PSChildName
+    
+    if (Test-Path $InterfacePath) {
+        $value = Get-ItemProperty -Path $InterfacePath -Name $valueName -ErrorAction SilentlyContinue
+        if ($value.valueName -eq $NewValue) {
+        Write-Host "$ValueName at $interfacePath is set to $NewValue as required."
+        }
+        else {
+        New-ItemProperty -Path $InterfacePath -Name $ValueName -Value $NewValue -PropertyType DWORD -Force
+        Write-Host "$ValueName on $Interface has been changed to $NewValue"
+        }
+      }
+    }
+}
+catch {
+    $errMsg = $_.Exception.Message
+    return $errMsg
+    Stop-Transcript
+    Exit 1
+    }
+
+Stop-Transcript
+Exit 0
