ac481d7ba5f8b2515349ac33beb5a0db4e2d345d
Andrew-c94 <andrew.currell@outlook.com>
2025-03-13 18:03:22 +0800

    Add files via upload

---
+++ SoftwareCenterShortcut-Detection.ps1
@@ -0,0 +1,53 @@
+﻿<#
+Script Name: SoftwareCenterShortcut-Detection.ps1
+Version: 2
+
+Description: This is a detection script used with remediations to detect whether the shortcut for MECM Software Center is visible in the start menu.
+If it is, the script exits with an exit code of 1 to trigger Intune to continue with the remediation script.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+#------------------------------------ Set Variables -------------------------------------#
+
+$logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\SoftwareCenterShortcut-Detection.log"
+$Shortcut = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Configuration Manager\Configuration Manager\Software Center.lnk"
+$ExpectedAttributes = 'NotContentIndexed','hidden'
+#This checks if the Software Center shortcut is visible in the start menu and proceeds to a remediation script to hide it if it is.
+
+#------------------------------------ Start log file ------------------------------------#
+
+Start-Transcript -Path $logfile
+function WriteLog
+{
+Param ([string]$LogString)
+
+$DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$LogMessage = "$Datetime $LogString"
+
+write-host $LogMessage
+}
+
+#----------------------- Check Software Centre Shortcut Visibility -------------------------#
+
+$matchFound = $false
+
+# Check the file attributes of the shortcut
+$ShortcutDetails = (Get-Item $Shortcut -force)
+$ShortcutStatus = $ShortcutDetails.Attributes
+if ($ShortcutStatus -eq $ExpectedAttributes) {
+      writelog "Shortcut has been hidden as expected."
+      $matchFound = $true
+}
+
+# Exit script if a match is found
+if ($matchFound -eq $false) {
+    writelog "Software Center shortcut is still visible, proceeding to hide."
+    Stop-Transcript
+    exit 1
+}
+if ($matchFound -eq $true) {
+    writelog ("Software Center shortcut hidden as expected, exiting script")
+    Stop-Transcript
+    exit 0
+}
---
+++ SoftwareCenterShortcut-Remediation.ps1
@@ -0,0 +1,59 @@
+﻿<#
+Script Name: SoftwareCenterShortcut-Remediation.ps1
+Version: 2
+
+Description: This is a remediation script used with remediations to hide the Software Center shortcut from the start menu.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+#------------------------------------ Set Variables -------------------------------------#
+
+$logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\SoftwareCenterShortcut-Remediation.log"
+$Shortcut = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Configuration Manager\Configuration Manager\Software Center.lnk"
+$ExpectedAttributes = 'NotContentIndexed','hidden'
+#This checks if the Software Center shortcut is present in the start menu and hides it if it is.
+$ErrorActionPreference = 'Stop'
+
+#------------------------------------ Start log file ------------------------------------#
+
+Start-Transcript -Path $logfile
+function WriteLog
+{
+Param ([string]$LogString)
+
+$DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$LogMessage = "$Datetime $LogString"
+
+write-host $LogMessage
+}
+
+#---------------------------- Hide Software Centre Shortcut  ------------------------------#
+
+# Check the file attributes of the shortcut
+$ShortcutDetails = (Get-Item $Shortcut -force)
+$ShortcutStatus = $ShortcutDetails.Attributes
+if ($ShortcutStatus -ne $ExpectedAttributes) 
+{
+writelog "Hiding shortcut."
+
+    # Set the shortcut to hidden
+    Try{
+        $ShortcutDetails.Attributes = $ExpectedAttributes
+        writelog "Shortcut hidden successfully"
+        Stop-Transcript
+        Exit 0
+    }
+    catch {
+        $errMsg = $_.Exception.Message
+        return $errMsg
+        Stop-Transcript
+        Exit 1
+    }
+}
+if ($ShortcutStatus -eq $ExpectedAttributes)
+{
+    writelog "Shortcut already hidden, no changes made."
+    Stop-Transcript
+    Exit 0
+}
