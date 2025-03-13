413159ccc380fff447ab4bab9317353d89c76845
Andrew-c94 <andrew.currell@outlook.com>
2025-03-13 17:59:27 +0800

    Add files via upload

---
+++ LSAAnonymousNameLookup-Detection.ps1
@@ -0,0 +1,56 @@
+﻿<#
+Script Name: LSAAnonymousNameLookup-Detection.ps1
+Version: 1
+
+Description: This is a detection script used with remediations to detect whether the 'Network access: Allow anonymous SID/Name translation'
+security policy is set to disabled.
+If it is not, the script exits with an exit code of 1 to trigger Intune to continue with the remediation script.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+#------------------------------------ Set Variables -------------------------------------#
+
+$logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LSAAnonymousNameLookup-Detection.log"
+$secpolicy = secedit /export /cfg $env:temp/secexport.cfg
+$LSAAnonymousNameLookupStatus = $(gc $env:temp/secexport.cfg | Select-String "LSAAnonymousNameLookup").ToString().Split('=')[1].Trim()
+$LSAAnonymousNameLookupValue = "0"
+#This sets the 'Network access: Allow anonymous SID/Name translation' policy setting is disabled.
+
+#------------------------------------ Start log file ------------------------------------#
+
+Start-Transcript -Path $logfile
+function WriteLog
+{
+Param ([string]$LogString)
+
+$DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$LogMessage = "$Datetime $LogString"
+
+write-host $LogMessage
+}
+
+#----------------------------------- Check LSA Status ---------------------------------------#
+
+$matchFound = $false
+
+# Check for existence of the policy setting in the local security policy and specific value
+if ($null -ne $LSAAnonymousNameLookupStatus -and $LSAAnonymousNameLookupStatus -eq $LSAAnonymousNameLookupValue) {
+    writelog "Local Security Policy setting with value $LSAAnonymousNameLookupValue found."
+    $matchFound = $true
+    }
+
+# Exit script if a match is found
+if ($matchFound -eq $false) {
+    writelog ("Local Security Policy setting with value $LSAAnonymousNameLookupValue does not exist, proceeding with script to set value")
+    Remove-Item $env:temp/secexport.cfg -force
+    Stop-Transcript
+    exit 1
+}
+if ($matchFound -eq $true) {
+    writelog ("Match Found, exiting script")
+    Remove-Item $env:temp/secexport.cfg -force
+    Stop-Transcript
+    exit 0
+}
+
---
+++ LSAAnonymousNameLookup-Remediation.ps1
@@ -0,0 +1,54 @@
+﻿<#
+Script Name: LSAAnonymousNameLookup-Remediation.ps1
+Version: 1
+
+Description: This is a remediation script used with remediations which disables the 'Network access: Allow anonymous SID/Name translation'
+security policy setting.
+
+The script is provided "AS IS" with no warranties.
+#>
+
+#------------------------------------ Set Variables -------------------------------------#
+
+$logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LSAAnonymousNameLookup-Remediation.log"
+$secpolicy = secedit /export /cfg $env:temp/secexport.cfg
+$LSAAnonymousNameLookupName = "LSAAnonymousNameLookup"
+$LSAAnonymousNameLookupStatus = $(gc $env:temp/secexport.cfg | Select-String $LSAAnonymousNameLookupName).ToString().Split('=')[1].Trim()
+$LSAAnonymousNameLookupValue = "0"
+#This sets the 'Network access: Allow anonymous SID/Name translation' policy setting is disabled.
+$ErrorActionPreference = 'Stop'
+
+#------------------------------------ Start log file ------------------------------------#
+
+Start-Transcript -Path $logfile
+function WriteLog
+{
+Param ([string]$LogString)
+
+$DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+$LogMessage = "$Datetime $LogString"
+
+write-host $LogMessage
+}
+
+#---------------------------------- Remediate LSA Status ----------------------------------#
+
+# Check for existence of the policy setting in the local security policy and specific value
+if ($LSAAnonymousNameLookupStatus -ne $LSAAnonymousNameLookupValue) {
+    
+    # Update Security Policy and catch errors if any
+    Try{
+        (gc $env:temp/secexport.cfg).replace("$LSAAnonymousNameLookupName = $LSAAnonymousNameLookupStatus", "$LSAAnonymousNameLookupName = $LSAAnonymousNameLookupValue") | Out-File $env:temp/secexport.cfg
+        secedit /configure /db c:\windows\security\local.sdb /cfg $env:temp/secexport.cfg /areas SECURITYPOLICY
+        Remove-Item $env:temp/secexport.cfg -force
+        writelog "Security Policy has been updated successfully"
+        Stop-Transcript
+        Exit 0
+    }
+    catch {
+        $errMsg = $_.Exception.Message
+        return $errMsg
+        Stop-Transcript
+        Exit 1
+    }
+}
