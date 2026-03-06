9435ce918384536bdd3b46404beaf6b8ac93cc58
Andrew Currell <andrew.currell@outlook.com>
2026-03-06 11:54:21 +0800

    Update NAble Update Settings to process multiple reg keys

--- Remediations/NAbleUpdateSettings-Detection.ps1
+++ Remediations/NAbleUpdateSettings-Detection.ps1
@@ -1,5 +1,6 @@
 <#
 Script Name: NAbleUpdateSettings-Detection.ps1
+Version: 2
 Author: Andrew Currell (GIT:@Andrew-c94)
 
 Description: This is a detection script used with remediations to detect whether old update settings deployed by 
@@ -10,17 +11,28 @@ If old settings are found, the script exits with an exit code of 1 to trigger In
 
 #------------------------------------ Set Variables -------------------------------------#
 
-$Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+$KeystoCheck = @(
+    @{
+    Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+    },
+    @{
+    Key = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UpdatePolicy\GPCache"
+    }
+)
 
 #---------------------------- Check Windows Update Settings -----------------------------#
 
-if (Test-Path $Key)
+ForEach ($RegKey in $KeystoCheck){
+    $keycheck = $RegKey.Key
+if (Test-Path $keycheck)
     {
-        Write-Host "Registry key $Key still exists, continuing to Remediation."
+        Write-Host "Registry key $keycheck still exists, continuing to Remediation."
         Exit 1
     }
 else {
-Write-Host "Registry key $key has already been removed, exiting without further action."
+Write-Host "$keycheck does not exist."
+}
+}
+Write-Host "All registry keys have already been removed, exiting without further action."
 Exit 0
-    }
 
--- Remediations/NAbleUpdateSettings-Remediation.ps1
+++ Remediations/NAbleUpdateSettings-Remediation.ps1
@@ -1,5 +1,6 @@
 <#
 Script Name: NableUpdateSettings-Remediation.ps1
+Version: 2
 Author: Andrew Currell (GIT:@Andrew-c94)
 
 Description: This is a remediation script used with remediations to remove Windows Update settings that are no
@@ -10,27 +11,40 @@ The script is provided "AS IS" with no warranties.
 
 #------------------------------------ Set Variables -------------------------------------#
 
-$KeytoCheck = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
-$KeytoDelete = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+$KeystoCheckandDelete = @(
+    @{
+    Keytocheck = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+    Keytodelete = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+    },
+    @{
+    Keytocheck = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UpdatePolicy\GPCache"
+    Keytodelete = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UpdatePolicy\GPCache"
+    }
+)
+
 $ErrorActionPreference = "Stop"
 
 #---------------------------- Remove Windpws Update Settings -------------------------------#
 
-Try{
-    if (Test-Path $KeytoCheck)
-    {
-    Write-Host "Registry key $KeytoCheck still exists, removing."
-    Remove-Item $KeytoDelete -Force -Recurse
+ForEach ($RegKey in $KeystoCheckandDelete){
+    $Keycheck = $RegKey.Keytocheck
+    $Keydelete = $RegKey.Keytodelete
+    Try{
+        if (Test-Path $Keycheck)
+        {
+        Write-Host "Registry key $Keycheck still exists, removing."
+        Remove-Item $Keydelete -Force -Recurse
+        }
+        else {
+        Write-Host "Registry key $Keycheck has already been removed."
+        }
     }
-    else {
-    Write-Host "Registry key $KeytoCheck has already been removed, exiting without further action."
-    }
-}
 catch {
     $errMsg = $_.Exception.Message
     Write-Host "An error has occured. ERROR:'$errMsg'. Exiting Script."
     Exit 1
     }
+}
 
-Write-Host "Settings cleaned up successfully."
+Write-Host "All reg keys cleaned up successfully."
 Exit 0
